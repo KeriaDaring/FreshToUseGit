cf7062e28c8ed7bd714fb469a3142adb71532a4f
lwj <973289634@qq.com>
2023-02-10 03:02:36 +0800

    The first commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int sum = 0;
+    for (int i = 0; i < 10; i++) {
+        sum += i;
+    }
+    printf("%d\n", sum);
+    printf("你完成了第一次在终端编译运行c语言代码\n");
+    return 0;
+}
