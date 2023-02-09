28266714467dd59a19a58b66369af580d6fc0e6c
Keria <973289634@qq.com>
2023-02-10 03:10:06 +0800

    more printing about circle

--- hello.c
+++ hello.c
@@ -7,5 +7,9 @@ int main() {
     }
     printf("%d\n", sum);
     printf("你完成了第一次在终端编译运行c语言代码\n");
+    for (int i = 0; i < 100; i++,i++) {
+        sum +=sum;
+    }
+    printf("%d\n", sum);
     return 0;
 }
